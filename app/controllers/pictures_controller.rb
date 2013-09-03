0707a48181b44208113768789a84f363647b038b
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-09-03 19:17:42 -0400

    Moving pictures into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,49 +1,13 @@
 class PicturesController < ApplicationController
 
 	def index
-
-		@pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
-
-
+		#indexes all pictures from the database
+		@pictures = Picture.all
 	end
 
 	def show
-
-		@pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-      }
-    ]
-
-    @picture = @pictures[params[:id].to_i]
+		#pulls single picture after searching the id number
+    	@picture = Picture.find[params[:id].to_i]
 	end
 
 
