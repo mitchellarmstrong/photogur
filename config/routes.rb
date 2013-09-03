940b172d452cb27a87ba1cc30087928cf4151e4e
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-09-03 18:19:39 -0400

    displays pictures and text

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Photogur::Application.routes.draw do
+
+
+
+	get 'pictures' => 'pictures#index'
+
+
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
