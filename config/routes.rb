fc6daad079cc7a034710d9bde680859654b8a19f
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-09-03 18:48:44 -0400

    form added

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Photogur::Application.routes.draw do
 
 	get 'pictures' => 'pictures#index'
 
+	post 'pictures' => "pictures#create"
+	get 'pictures/new' => 'pictures#new'
+
 	get 'pictures/:id' => 'pictures#show', as: "picture"
-
-
-
-
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
