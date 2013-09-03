dc23d942ffccd9b4b3613e6ace864c5ad1ecebc3
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-09-03 18:29:19 -0400

    display individual photos

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Photogur::Application.routes.draw do
 
 	get 'pictures' => 'pictures#index'
 
+	get 'pictures/0' => 'pictures#picture0'
+	get 'pictures/1' => 'pictures#picture1'
+	get 'pictures/2' => 'pictures#picture2'
+
 
 
 
